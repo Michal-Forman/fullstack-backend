7fcc5f7a5d0418a743e9609d49f0883ba1552e54
michalforman <miska.forman@gmail.com>
2023-07-07 19:11:53 +0200

    allow cors from frontend

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 
 const app = express();
+
+app.use(cors({
+    origin: 'https://michal-forman.github.io',
+}));
 
 app.get("/api", (req, res) => {
     res.json({"users": ["user1", "user2", "user3"]});
